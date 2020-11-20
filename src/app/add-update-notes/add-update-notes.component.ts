2de769c3dbfe805d78ce5706c46a3084ae8f5d69
placidowang <placidow@gmail.com>
2020-11-20 02:02:04 -0600

    update

---
+++ src/app/add-update-notes/add-update-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-update-notes',
+  templateUrl: './add-update-notes.component.html',
+  styleUrls: ['./add-update-notes.component.css']
+})
+export class AddUpdateNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/notes-listing/notes-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-listing',
+  templateUrl: './notes-listing.component.html',
+  styleUrls: ['./notes-listing.component.css']
+})
+export class NotesListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
